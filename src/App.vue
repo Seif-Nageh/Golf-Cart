5d633a8c1574d3d51e3a3603795b70f5f3dc6128
Seif Nageh <seifldin.attallah@gmail.com>
2023-06-06 11:44:33 -0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,13 @@
 <script setup>
 import { RouterView } from "vue-router";
+import WhatsappButton from "./components/WhatsappButton.vue";
 // import LoadingModalComponent from "./components/LoadingModalComponent.vue";
-import { SocialChat } from "vue-social-chat";
-
-const attendants = [
-  {
-    app: "whatsapp",
-    label: "Technical support",
-    name: "Naif Abdallah",
-    number: "5581983383532",
-    avatar: {
-      src: "https://avatars0.githubusercontent.com/u/8084606?s=460&u=20b6499a416cf7129a18e5c168cf387e159edb1a&v=4",
-      alt: "",
-    },
-  },
-];
 </script>
 
 <template>
   <!-- <LoadingModalComponent></LoadingModalComponent> -->
   <div class="min-h-screen">
     <RouterView />
-    <SocialChat icon :attendants="attendants">
-      <template #header>
-        <p>Click on one of our attendants below to chat on WhatsApp.</p>
-      </template>
-      <template #button>
-        <font-awesome-icon size="2xl" :icon="`fa-brands fa-whatsapp`" />
-      </template>
-      <template #footer>
-        <small>Opening hours: 8am to 6pm</small>
-      </template>
-    </SocialChat>
+    <WhatsappButton />
   </div>
 </template>
---
+++ src/components/WhatsappButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { SocialChat } from "vue-social-chat";
+
+const attendants = [
+  {
+    app: "whatsapp",
+    label: "Technical support",
+    name: "Naif Abdallah",
+    number: "5581983383532",
+    avatar: {
+      src: "https://avatars0.githubusercontent.com/u/8084606?s=460&u=20b6499a416cf7129a18e5c168cf387e159edb1a&v=4",
+      alt: "",
+    },
+  },
+];
+</script>
+
+<template>
+  <SocialChat icon :attendants="attendants">
+    <template #header>
+      <p>Click on one of our attendants below to chat on WhatsApp.</p>
+    </template>
+    <template #button>
+      <font-awesome-icon size="2xl" :icon="`fa-brands fa-whatsapp`" />
+    </template>
+    <template #footer>
+      <small>Opening hours: 8am to 6pm</small>
+    </template>
+  </SocialChat>
+</template>
