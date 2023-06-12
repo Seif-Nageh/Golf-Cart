ddeb366deb0c638020f797efe454e5cba62e0ca7
Seif Nageh <seifldin.attallah@gmail.com>
2023-06-12 13:23:57 +0300

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { RouterView } from "vue-router";
 import WhatsappButton from "./components/WhatsappButton.vue";
-// import LoadingModalComponent from "./components/LoadingModalComponent.vue";
+import LoadingModalComponent from "./components/LoadingModalComponent.vue";
+import { ref } from "vue";
+
+const loading = ref(true);
+
+window.addEventListener("load", () => {
+  loading.value = false;
+});
 </script>
 
 <template>
-  <!-- <LoadingModalComponent></LoadingModalComponent> -->
-  <div class="min-h-screen">
+  <div class="min-h-screen transition-all duration-300 ease-in-out">
+    <LoadingModalComponent v-if="loading" class=""></LoadingModalComponent>
     <RouterView />
     <WhatsappButton />
   </div>
--- src/components/LoadingModalComponent.vue
+++ src/components/LoadingModalComponent.vue
@@ -1,3 +1,10 @@
 <template>
-  <div class="bg-primary-400 absolute z-50 inset-0"></div>
+  <div
+    class="bg-white fixed z-[1000] inset-0 flex flex-col justify-center items-center"
+  >
+    <div
+      class="border-4 border-t-transparent border-r-primary-500/30 border-b-primary-500/60 w-12 h-12 border-primary-500 rounded-full animate-spin"
+    ></div>
+    <p class="text-4xl animate-pulse pt-6">Loading.....</p>
+  </div>
 </template>
